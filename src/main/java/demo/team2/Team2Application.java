170bc6821e056350db195c4ba81c5fe4d1e26eac
Matt Payne <Payne@MattPayne.org>
2023-07-21 08:35:58 -0500

    Hello CommandLineRunner

--- src/main/java/demo/team2/Team2Application.java
+++ src/main/java/demo/team2/Team2Application.java
@@ -1,14 +1,20 @@
 package demo.team2;
 
-import org.springframework.boot.SpringApplication;
+import static demo.team2.Utils.log;
+
+import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class Team2Application {
+public class Team2Application implements CommandLineRunner {
 
     public static void main(final String[] args) {
         SpringApplication.run(Team2Application.class, args);
+    }
+
+    public void run(String... args) {
+      log("Hello Command Line Runner!");
     }
 
 }
---
+++ src/main/java/demo/team2/Utils.java
@@ -0,0 +1,9 @@
+package demo.team2;
+
+public class Utils {
+  public static void log(String msg) {
+    System.out.println(msg);
+  }
+}
+
+
